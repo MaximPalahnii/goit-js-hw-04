0c21ea8dd31b4bbbf4303ed53aaed28ece3112ea
MaximPalahnii <miks81@ukr.net>
2020-09-09 13:50:08 +0300

    DZ-04 olldan

--- js/task_04.js
+++ js/task_04.js
@@ -4,14 +4,14 @@ const account = {
   discount: 0.1,
   orders: ['order-1', 'order-2', 'order-3'],
   changeDiscount(value) {
-    discount = this.value; // Write code in this line
+    this.discount = value; // Write code in this line
   },
   showOrders() {
     return this.orders; // Write code in this line
   },
   addOrder(cost, order) {
-    balance -= this.cost; // Write code in this line
-    orders.push(this.order); // Write code in this line
+    this.balance -= cost; // Write code in this line
+    this.orders.push(order); // Write code in this line
   },
 };
 const copyAccount = Object.assign({}, account);
@@ -19,14 +19,13 @@ copyAccount.orders = [...account.orders];
 // копируем для автотестов ссылочные типы
 
 account.changeDiscount(0.15);
-console.log(account.discount);
-0.15;
+console.log(account.discount); // 0.15
 
 console.log(account.showOrders());
-['order-1', 'order-2', 'order-3'];
+// ['order-1', 'order-2', 'order-3']
 
 account.addOrder(5000, 'order-4');
 console.log(account.balance); // 19000
 
 console.log(account.showOrders());
-['order-1', 'order-2', 'order-3', 'order-4'];
+// ['order-1', 'order-2', 'order-3', 'order-4']
